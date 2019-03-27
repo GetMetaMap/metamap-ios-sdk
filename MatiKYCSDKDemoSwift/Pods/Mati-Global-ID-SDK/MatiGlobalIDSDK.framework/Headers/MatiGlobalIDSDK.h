2ed75276c220bb8ce8c7797837a85d931876e768
surenpogh <poghosyan.suren.1998@gmail.com>
2019-03-27 15:29:43 +0400

    added demo app to help clients integrate our Mati iOS SDK

---
+++ MatiKYCSDKDemoSwift/Pods/Mati-Global-ID-SDK/MatiGlobalIDSDK.framework/Headers/MatiGlobalIDSDK.h
@@ -0,0 +1,19 @@
+//
+//  MatiGlobalIDSDK.h
+//  MatiGlobalIDSDK
+//
+//  Created by Musho Hovhannisyan on 6/18/18.
+//  Copyright © 2018 Mati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MatiGlobalIDSDK.
+FOUNDATION_EXPORT double mati_global_id_sdkVersionNumber;
+
+//! Project version string for MatiGlobalIDSDK.
+FOUNDATION_EXPORT const unsigned char mati_global_id_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mati_kyc_sdk/PublicHeader.h>
+
+
