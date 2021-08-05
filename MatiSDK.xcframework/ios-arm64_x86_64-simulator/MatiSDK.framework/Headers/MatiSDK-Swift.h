31a65aac8f915a33b2eb77914d8fee785f905c60
andrey.posnov <andrey.posnov@mati.io>
2021-08-05 23:45:47 +0300

    fix localizable strings issue

--- MatiSDK.xcframework/ios-arm64_x86_64-simulator/MatiSDK.framework/Headers/MatiSDK-Swift.h
+++ MatiSDK.xcframework/ios-arm64_x86_64-simulator/MatiSDK.framework/Headers/MatiSDK-Swift.h
@@ -563,8 +563,6 @@ SWIFT_CLASS("_TtC7MatiSDK13SocketRawView")
 
 
 
-
-
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -1121,8 +1119,6 @@ SWIFT_CLASS("_TtC7MatiSDK13SocketRawView")
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
-
-
 
 
 
