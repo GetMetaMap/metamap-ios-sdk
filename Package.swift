a4b4aeac1e06192f409451a3488eaebab0e73643
andrey.posnov <andrey.posnov@mati.io>
2021-09-14 12:58:33 +0300

    Swift package manager support added

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-framework",
+    name: "MatiSDK",
     platforms: [.iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ios-framework",
-            targets: ["ios-framework"]),
+            name: "MatiSDK",
+            targets: ["MatiSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ios-framework",
+            name: "MatiSDK",
             dependencies: [
                 "MatiSDK"]),
         .binaryTarget(name: "MatiSDK", path: "MatiSDK.xcframework")
