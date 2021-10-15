ea4b05a3a2b34bf963996c6059c08948d1793397
Аво <avetik.sukiasyan@mati.io>
2021-10-15 15:28:41 +0400

    version 3.7.0

--- MatiSDK.xcframework/ios-arm64_x86_64-simulator/MatiSDK.framework/Headers/MatiSDK-Swift.h
+++ MatiSDK.xcframework/ios-arm64_x86_64-simulator/MatiSDK.framework/Headers/MatiSDK-Swift.h
@@ -568,6 +568,8 @@ SWIFT_CLASS("_TtC7MatiSDK13SocketRawView")
 
 
 
+
+
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
 #endif
@@ -1124,6 +1126,8 @@ SWIFT_CLASS("_TtC7MatiSDK13SocketRawView")
 - (nonnull instancetype)init SWIFT_UNAVAILABLE;
 + (nonnull instancetype)new SWIFT_UNAVAILABLE_MSG("-init is unavailable");
 @end
+
+
 
 
 
